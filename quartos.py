31a4a19b2fe29f78de1743c5c4d59af5a95a1d74
Maria <maria_caseiro@outlook.com>
2026-02-20 16:49:01 +0000

    Criação de função para listar quartos

--- quartos.py
+++ quartos.py
@@ -3,3 +3,11 @@ class Quarto:
         self.numero = numero
         self.tipo = tipo
         self.disponivel = True
+
+    def listagem_quartos(self):
+        for q in self.quartos:
+            print(f"-------- LISTA DE QUARTOS --------\nNúmero: {q.numero}\nTipo: {q.tipo}")
+    
+quarto1 = Quarto(1, "Individual")
+quarto2 = Quarto(2, "Duplo")
+quarto3 = Quarto(3, "Casal")
